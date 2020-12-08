a1691fc30fa95e23823b42bd5808ba85e8c67869
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2020-12-08 16:22:13 +0700

    COMMIT : Destination screen half done

---
+++ dicoding_flut_pemula_subm1_v1/lib/screens/destination_screen.dart
@@ -0,0 +1,120 @@
+import 'package:dicoding_flut_pemula_subm1_v1/models/destination_models.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class DestinationScreen extends StatefulWidget {
+  final Destination destination;
+
+  DestinationScreen({this.destination});
+
+  @override
+  _DestinationScreenState createState() => _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6.0,
+                    )
+                  ],
+                ),
+                child: Hero(
+                  tag: widget.destination.imageUrl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image(
+                      image: AssetImage(widget.destination.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 40.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 30.0,
+                      color: Colors.black,
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        IconButton(
+                          icon: Icon(FontAwesomeIcons.sortAmountDown),
+                          iconSize: 25.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 20.0,
+                bottom: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.destination.city,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.locationArrow,
+                          size: 10.0,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          widget.destination.country,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- dicoding_flut_pemula_subm1_v1/lib/widgets/hotel_carousel.dart
+++ dicoding_flut_pemula_subm1_v1/lib/widgets/hotel_carousel.dart
@@ -1,4 +1,4 @@
-import 'package:dicoding_flut_pemula_subm1_v1/models/destination_models.dart';
+import 'package:dicoding_flut_pemula_subm1_v1/models/hotel_models.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -12,7 +12,7 @@ class HotelCarousel extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text('Top Destinations',
+              Text('Exclusive Hotels',
                   style: TextStyle(
                       fontSize: 22.0,
                       fontWeight: FontWeight.bold,
@@ -33,12 +33,12 @@ class HotelCarousel extends StatelessWidget {
           height: 300.00,
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: destinations.length,
+              itemCount: hotels.length,
               itemBuilder: (BuildContext context, int index) {
-                Destination destination = destinations[index];
+                Hotel hotel = hotels[index];
                 return Container(
                   margin: EdgeInsets.all(10.0),
-                  width: 210.0,
+                  width: 240.0,
                   child: Stack(
                     alignment: Alignment.topCenter,
                     children: <Widget>[
@@ -54,18 +54,26 @@ class HotelCarousel extends StatelessWidget {
                             padding: const EdgeInsets.all(10.0),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
-                                  '${destination.activities.length} activities',
+                                  hotel.name,
                                   style: TextStyle(
                                       fontSize: 22.0,
                                       fontWeight: FontWeight.w600,
                                       letterSpacing: 1.2),
                                 ),
+                                SizedBox(height: 2.0),
                                 Text(
-                                  destination.description,
+                                  hotel.address,
                                   style: TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(height: 2.0),
+                                Text(
+                                  '\$${hotel.price} / night',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ],
                             ),
@@ -90,47 +98,11 @@ class HotelCarousel extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20.0),
                               child: Image(
                                 height: 180.0,
-                                width: 180.0,
-                                image: AssetImage(destination.imageUrl),
+                                width: 220.0,
+                                image: AssetImage(hotel.imageUrl),
                                 fit: BoxFit.cover,
                               ),
                             ),
-                            Positioned(
-                              left: 10.0,
-                              bottom: 10.0,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    destination.city,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 24.0,
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 1.2,
-                                    ),
-                                  ),
-                                  Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        FontAwesomeIcons.locationArrow,
-                                        size: 10.0,
-                                        color: Colors.white,
-                                      ),
-                                      SizedBox(
-                                        width: 5.0,
-                                      ),
-                                      Text(
-                                        destination.country,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                            )
                           ],
                         ),
                       ),
